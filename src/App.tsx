539979b65137a0f36daaa0a5d0b6b8943ffe7693
Skanda <skandakk@gmail.com>
2020-05-04 20:12:40 -0700

    added hospitalmap component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import HospitalSearch from './components/HospitalSearch';
+import HospitalMap from './components/HospitalMap';
 import { render } from '@testing-library/react';
 
 class App extends React.Component {
@@ -16,6 +17,7 @@ class App extends React.Component {
               <HospitalSearch />
             </Route>
           </Switch>
+          <HospitalMap />
         </main>
       </Router>
     );
