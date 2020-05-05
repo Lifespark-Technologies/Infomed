934fbf2f79518f00f80054a6919d87a00be68c0c
Skanda <skandakk@gmail.com>
2020-05-04 19:32:30 -0700

    Restructured from function declaration to class declaration for readability

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,23 @@ import './App.css';
 import Header from './components/Header';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import HospitalSearch from './components/HospitalSearch';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <Router>
-      <Header />
-      <main>
-        <Switch>
-          <Route path="/">
-            <HospitalSearch />
-          </Route>
-        </Switch>
-      </main>
-    </Router>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Header />
+        <main>
+          <Switch>
+            <Route path="/">
+              <HospitalSearch />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    );
+  }
 }
 
 export default App;
