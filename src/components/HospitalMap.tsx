befd1ef5976c49a161f78fc844e5090f515745a0
Skanda <skandakk@gmail.com>
2020-05-04 20:13:00 -0700

    Added hospitalmap.tsx component...map doesn't render for some reason

---
+++ src/components/HospitalMap.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import * as L from 'leaflet';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+
+type State = {
+  lat: number,
+  lng: number,
+  zoom: number,
+}
+
+class HospitalMap extends React.Component {
+
+  state: State = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13,
+  }
+
+  render() {
+    const position: L.LatLng = L.latLng(this.state.lat, this.state.lng);
+    return (
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    );
+  }
+}
+
+
+export default HospitalMap;
