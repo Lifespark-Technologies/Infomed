5a82e0eff3fe416aa9575b59c2da5889e8605f5e
Ameen <crafterandcoder@gmail.com>
2020-05-07 22:25:09 +0300

    Played with nav
    Centered it horizontally and vertically , added on hover and  yeah that's all , jt current color is yellow but can changed later on .... also i think we can have bg of nav as grey on hover it turns white 
    
    Also a zooming effect on hover
    
    My first commit :)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <header className={styles.header}>
-      <div>ICULive</div>
+      <div class={styles.nav}>ICULive</div>
       <nav>
         <ul className={styles.navList}>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/hospitals">Hospitals</Link></li>
-          <li><Link to="/">Contact</Link></li>
+          <li class={styles.nav}><Link to="/">Home</Link></li>
+          <li class={styles.nav}><Link to="/hospitals">Hospitals</Link></li>
+          <li class={styles.nav}><Link to="/">Contact</Link></li>
         </ul>
       </nav>
     </header>
