b7c5045b7346a30bca9a3979f3170f31c4173c63
naijauser <unix1gl@gmail.com>
2020-05-09 21:23:37 +0100

    moved leaflet container height declaration to index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './index.css';
 import Header from './components/Header';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import HospitalSearch from './components/HospitalSearch';
