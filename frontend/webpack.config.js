93a954e412429fd0d743881ce515c3edc47b8ae3
Skanda <skandakk@gmail.com>
2020-05-11 01:43:00 -0700

    Trying to render css in server

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,3 +1,5 @@
+// const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   module: {
     rules: [
@@ -11,10 +13,24 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
       }
     ]
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx']
   }
+  // plugins: [
+  //   new HtmlWebPackPlugin({
+  //     template: "./templates/frontend/index.html", // template html file
+  //     filename: "./index.html" // actual html file that it's supposed to render
+  //   })
+  // ]
 };
