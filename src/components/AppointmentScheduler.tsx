850606a48bf364137162e6b61c43629f6ce62805
Bartosz Leper <bl.nero@gmail.com>
2020-05-13 18:44:02 +0200

    Address review

--- src/components/AppointmentScheduler.tsx
+++ src/components/AppointmentScheduler.tsx
@@ -5,35 +5,56 @@ import AppointmentTimePicker from "./AppointmentTimePicker";
 import { format, isSameDay } from "date-fns";
 import { useParams } from "react-router-dom";
 
-enum Stage { Loading, SelectDate, SelectTime, Confirmed };
+/** 
+ * Determines the stage of user's interaction with [[`AppointmentScheduler`]].
+ */
+enum Stage {
+  /** Loading available slots from the server. */
+  Loading,
+  /** User is picking available date from the calendar. */
+  SelectDate,
+  /** User is picking available time slot within the selected date. */
+  SelectTime,
+  /** All set, showing confirmation screen. */
+  Confirmed,
+};
 
 interface AppointmentSchedulerParams {
   hospitalId: string;
 }
 
+/**
+ * Displays a UI that allows user to schedule an appointment by providing date,
+ * time slot, and email. Available time slots are fetched from the server,
+ * basing on [[`AppointmentSchedulerParams.hospitalId`]].
+ */
 export const AppointmentScheduler = ({ hospitalId }: AppointmentSchedulerParams) => {
+  // Mutable control state.
   const [availableSlots, setAvailableSlots] = useState<AppointmentSlot[]>([]);
   const [stage, setStage] = useState(Stage.Loading);
   const [datePicked, setDatePicked] = useState(new Date());
   const [appointmentSlot, setAppointmentSlot] = useState<AppointmentSlot | null>(null);
   const [email, setEmail] = useState('');
 
+  // This effect is responsible for fetching available appointment slots from
+  // the server.
   useEffect(() => {
     (async () => {
-      // Fetch appointment slots from the server.
-      // TODO(@bl-nero): Change dates along when user changes month in the calendar.
+      // TODO(@bl-nero): Change dates whenever user changes month in the calendar.
       const slots = await fetchAppointmentSlots(
         hospitalId, new Date(2020, 4, 1), new Date(2020, 6, 1));
       setAvailableSlots(slots);
       setStage(Stage.SelectDate);
     })();
-  }, [hospitalId]);
+  }, [hospitalId] /* Fetch whenever hospitalId changes. */);
 
+  // Switch to time slot selection once user picks date.
   const pickDate = (date: Date) => {
     setDatePicked(date);
     setStage(Stage.SelectTime);
   };
 
+  // Once user confirms, schedule an appointment.
   const schedule = async () => {
     if (!appointmentSlot) {
       return;
@@ -42,46 +63,43 @@ export const AppointmentScheduler = ({ hospitalId }: AppointmentSchedulerParams)
     setStage(Stage.Confirmed);
   }
 
-  return (
-    <>
-      {stage === Stage.Loading &&
-        <div>
-          Loading
-        </div>
-      }
+  switch (stage) {
+    case Stage.Loading: return (
+      <div>
+        Loading
+      </div>
+    );
 
-      {stage === Stage.SelectDate &&
-        <div>
-          Please select a date
-          <AppointmentDatePicker availableSlots={availableSlots} onDatePicked={pickDate} />
-        </div>
-      }
-
-      {stage === Stage.SelectTime &&
-        <div>
-          <p>You’ve selected {format(datePicked, 'PPP')}</p>
-          <p>Please select a time for your appointment</p>
-          <AppointmentTimePicker
-            timeSlots={availableSlots.filter(slot => isSameDay(datePicked, slot.start))}
-            onChange={setAppointmentSlot}
-          />
-          <label>
-            What’s your email address?
+    case Stage.SelectDate: return (
+      <div>
+        Please select a date
+        <AppointmentDatePicker availableSlots={availableSlots} onDatePicked={pickDate} />
+      </div>
+    );
+    case Stage.SelectTime: return (
+      <div>
+        <p>You’ve selected {format(datePicked, 'PPP')}</p>
+        <p>Please select a time for your appointment</p>
+        <AppointmentTimePicker
+          timeSlots={availableSlots.filter(slot => isSameDay(datePicked, slot.start))}
+          onChange={setAppointmentSlot}
+        />
+        <label>
+          What’s your email address?
             <input value={email} onChange={e => setEmail(e.target.value)} />
-          </label>
-          <button disabled={!appointmentSlot || !email} onClick={schedule}>
-            Done
+        </label>
+        <button disabled={!appointmentSlot || !email} onClick={schedule}>
+          Done
           </button>
-        </div>
-      }
+      </div>
+    );
 
-      {stage === Stage.Confirmed &&
-        <div>
-          Great, you’ve booked your appointment. A confirmation email has been send to you.
-        </div>
-      }
-    </>
-  );
+    case Stage.Confirmed: return (
+      <div>
+        Great, you’ve booked your appointment. A confirmation email has been send to you.
+      </div>
+    );
+  }
 }
 
 export default () => {
