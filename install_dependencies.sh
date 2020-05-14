c3335b5045dd2f122cf9cb315802ef2a5f3d4e5d
Skanda <skandakk@gmail.com>
2020-05-13 21:39:59 -0700

    Added an install dependencies script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,11 +3,19 @@
 
 
 echo "WARNING: This script requires root access to install dependencies."
-echo "Do you still want to continue? [Y/n]"
-read yn
+
+read -p "Do you still want to continue? [Y/n] " yn
+
 if [ $yn = "Y" ] || [ $yn = "y" ]; then
 	sudo apt install gdal-bin libgdal-dev
 	sudo apt install python3-gdal
 	sudo apt install binutils libproj-dev
+fi
+
+read -p "Have you installed Docker? [Y/n] " yn
+
+# This sets up the Postgres docker container.
+if [ $yn = "Y" ] || [ $yn = "y" ]; then
+	docker run --name=postgis -d -e POSTGRES_USER=user001 -e POSTGRES_PASS=123456789 -e POSTGRES_DBNAME=gis -p 5432:5432 kartoza/postgis:9.6-2.4
 fi
 
