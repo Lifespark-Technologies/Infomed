9ca34e88f8a5ca383a730191290186e5878c5774
Skanda Kaashyap <skandakk@gmail.com>
2020-06-01 18:00:55 -0500

    Finished writing the appointmentSlots get endpoint

--- hospitals/views.py
+++ hospitals/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Hospital, AppointmentSlot
 from .serializers import HospitalSerializer, AppointmentSlotSerializer
+import dateutil.parser
 
 # Create your views here.
 
@@ -14,12 +15,24 @@ class HospitalView(viewsets.ModelViewSet):
     @action(detail=True, methods=["get"])
     def appointmentSlots(self, request, pk):
         hospital = self.get_object()
-        dat = hospital.appointment_slots.all()
-        serializer = AppointmentSlotSerializer(dat, many=True)
+        appointments = hospital.appointment_slots.all()
+
+        valid_appointments = []
+
+        if request.query_params:
+
+            start = dateutil.parser.parse(request.query_params["start"])
+            end = dateutil.parser.parse(request.query_params["end"])
+
+            for appointment in appointments:
+                if appointment.start.day >= start.day and appointment.end.day <= end.day:
+                    valid_appointments.append(appointment)
+
+        else:
+            valid_appointments = appointments
+
+
+        serializer = AppointmentSlotSerializer(valid_appointments, many=True)
         return Response(serializer.data)
-
-
-
-
 
 
