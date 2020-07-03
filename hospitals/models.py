adcd941865a3bd5bfcf535b06ef2b9ab0681cc54
Skanda Kaashyap <skandakk@gmail.com>
2020-07-03 18:59:02 -0500

    Updated hospital models

--- hospitals/models.py
+++ hospitals/models.py
@@ -10,6 +10,26 @@ class Hospital(models.Model):
     """
     name = models.CharField(max_length=50)
     location = models.PointField()
+    specialty = models.CharField(max_length=50, default="")
+
+    # Numerical fields
+    # Initializing all the hospital fields cited in this issue:
+    # https://github.com/Lifespark-Technologies/Infomed/issues/64
+    n95_masks = models.IntegerField(default=0)
+    three_layer_masks = models.IntegerField(default=0)
+    PPE_kits, PPE_kits_daily = models.IntegerField(default=0), models.IntegerField(default=0)
+    sanitizer = models.IntegerField(default=0)
+    bleaching_powder = models.FloatField(default=0.0)
+    sodium_hypochlorite = models.FloatField(default=0.0)
+    chemical_gloves = models.IntegerField(default=0)
+    infrared_thermometer = models.IntegerField(default=0)
+    handwash = models.FloatField(default=0.0)
+    viral_transport_medium = models.IntegerField(default=0)
+    swap_sticks = models.IntegerField(default=0)
+    three_layer_packing_mask = models.IntegerField(default=0)
+    ice_gel_pack = models.IntegerField(default=0)
+    rubbing_alcohol = models.FloatField(default=0)
+
 
     def __str__(self):
         return self.name
