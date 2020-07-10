661470e3cea4e9ec324c7e16faa9efaacbbc25c1
Skanda Kaashyap <skandakk@gmail.com>
2020-07-09 21:35:52 -0500

    Updated workflows to build the frontend directory

--- useful_scripts/install_dependencies.sh
+++ useful_scripts/install_dependencies.sh
@@ -26,6 +26,4 @@ if [ $yn = "Y" ] || [ $yn = "y" ]; then
 	docker run --name=postgis -d -e POSTGRES_USER=user001 -e POSTGRES_PASS=123456789 -e POSTGRES_DBNAME=gis -p 5432:5432 kartoza/postgis:9.6-2.4
 fi
 
-echo "Building frontend directory"
-cd ../frontend && npm run build
 
