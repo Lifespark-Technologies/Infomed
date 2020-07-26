8e09028515f8f3eb4189cea7761fffebdfd0b23e
Skanda Kaashyap <skandakk@gmail.com>
2020-07-26 03:55:38 -0500

    registered AppointmentSlot model to administration console

--- hospitals/admin.py
+++ hospitals/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Hospital
+from .models import Hospital, AppointmentSlot
 
 # Register your models here.
 
 class HospitalAdministration(admin.ModelAdmin):
     pass
 
+class AppointmentSlotAdministration(admin.ModelAdmin):
+    pass
+
 admin.site.register(Hospital, HospitalAdministration)
+admin.site.register(AppointmentSlot, AppointmentSlotAdministration)
