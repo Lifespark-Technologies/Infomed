7a896358748b3d823ed2336e71c595b7910fe7a6
Skanda Kaashyap <skandakk@gmail.com>
2020-07-28 15:52:11 -0500

    Fixed extra urls declaration

--- infomed/urls.py
+++ infomed/urls.py
@@ -21,7 +21,6 @@ from django.urls import path, include
 # they must appear as the last entry.
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
     path('', include('hospitals.urls')),
     path('', include('frontend.urls')),
 ]
