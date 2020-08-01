f909627d2f81077b2facb323539aa804a005978a
Skanda Kaashyap <skandakk@gmail.com>
2020-08-01 14:37:26 -0500

    Fixed Django admin for the users --- passwords are no longer stored unhashed

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,24 +1,70 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account, HospitalStaff, GeneralUser, HospitalAdmin
 from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from .forms import (
+    AccountCreationForm, 
+    GeneralUserCreationForm, 
+    HospitalAdminCreationForm, 
+    HospitalStaffCreationForm,
+    AccountModifyForm,
+)
 
-# Register your models here.
-class AccountAdmin(admin.ModelAdmin):
+
+class AccountAdmin(UserAdmin):
+
+    form = AccountModifyForm
+    add_form = AccountCreationForm
+
     list_display = ("email", "date_joined", "last_login",)
     search_fields = ("email",)
     readonly_fields = ("date_joined", "last_login")
+    list_filter = ("is_staff", "is_superuser",)
+
+    fieldset_fields = ("email", "is_superuser", "is_staff", "hospital")
+    
+    fieldsets = (
+        (None, {"fields": fieldset_fields}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password"),
+        }),
+    )
+
+    ordering = ()
+    filter_horizontal = ()
 
 
 class GeneralUserAdmin(AccountAdmin):
+
+    add_form = GeneralUserCreationForm
+
     list_display = AccountAdmin.list_display + ("doctor",)
     search_fields = AccountAdmin.search_fields + ("doctor",)
+    
+    fieldsets = (
+        (None, {"fields": AccountAdmin.fieldset_fields + ("doctor",)}),
+    )
+
 
 class HospitalStaffAdmin(AccountAdmin):
+
+    add_form = HospitalStaffCreationForm
+
     list_display = AccountAdmin.list_display + ("is_doctor",)
     search_fields = AccountAdmin.search_fields + ("is_doctor",)
 
+    fieldsets = (
+        (None, {"fields": AccountAdmin.fieldset_fields + ("is_doctor",)}),
+    )
+
 
 class HospitalAdminAdmin(AccountAdmin):
+
+    add_form = HospitalAdminCreationForm
     pass
 
 
